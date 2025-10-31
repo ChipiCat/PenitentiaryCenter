a153a1ef590d5ab67d4f329d98c8fad54832b7eb
gabygabylagaby <gabrielagv7656@gmail.com>
2025-10-31 02:08:35 -0400

    feat: add profile profile and user items change for data

--- peny-back/src/common/interfaces/entity.interface.ts
+++ peny-back/src/common/interfaces/entity.interface.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 
 export interface IPaginatedResponse<T> {
-  items: T[];
+  data: T[];
   page: number;
   size: number;
   total: number;
